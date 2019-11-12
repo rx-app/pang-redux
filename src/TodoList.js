36d4e9062b48de48697fad79ac621e7c5cf5dbbd
rx-app <rxapp@yandex.com>
2019-11-12 20:14:36 +0800

    读取state

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input , Button , List } from 'antd'
+import store from './store'
 
-const data=[
-    '早8点开晨会，分配今天的开发工作',
-    '早9点和项目经理作开发需求讨论会',
-    '晚5:30对今日代码进行review'
-]
+
 
 class TodoList extends Component {
+constructor(props){
+    super(props)
+    //关键代码-----------start
+    this.state=store.getState();
+    //关键代码-----------end
+    console.log(this.state)
+}
     render() { 
         return ( 
             <div style={{margin:'10px'}}>
                 <div>
-                    <Input placeholder='write someting' style={{ width:'250px', marginRight:'10px'}}/>
+                    
+                    <Input placeholder={this.state.inputValue} style={{ width:'250px', marginRight:'10px'}}/>
                     <Button type="primary">增加</Button>
                 </div>
-                {/* <div style={{margin:'10px',width:'300px'}}>
+                <div style={{margin:'10px',width:'300px'}}>
                     <List
                         bordered
-                        dataSource={data}
+                        //关键代码-----------start
+                        dataSource={this.state.list}
+                        //关键代码-----------end
                         renderItem={item=>(<List.Item>{item}</List.Item>)}
                     />    
-                </div> */}
+                </div>
             </div>
          );
     }

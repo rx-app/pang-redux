7e39eb89828d301818260525335509344464efa2
rx-app <rxapp@yandex.com>
2019-11-14 15:33:55 +0800

    把UI组件修改成无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -2,27 +2,24 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 
-class TodoList extends Component {
-    
-    render() {
-        return (
-            <div style={{ margin: '10px' }}>
-                <div>
+const TodoList = (props) => {
+    return (
+        <div style={{ margin: '10px' }}>
+            <div>
 
-                    <Input onChange={this.props.changeInputValue} placeholder={this.props.inputValue} style={{ width: '250px', marginRight: '10px' }} />
-                    <Button onClick={this.props.clickBtn} type="primary">增加</Button>
-                </div>
-                <div style={{ margin: '10px', width: '300px' }}>
-                    <List
-                        bordered
-                        //关键代码-----------start
-                        dataSource={this.props.list}
-                        //关键代码-----------end
-                        renderItem={(item,index) => (<List.Item onClick={()=>this.props.deleteItem(index)} >{item}</List.Item>)}
-                    />
-                </div>
+                <Input onChange={props.changeInputValue} placeholder={props.inputValue} style={{ width: '250px', marginRight: '10px' }} />
+                <Button onClick={props.clickBtn} type="primary">增加</Button>
             </div>
-        );
-    }
+            <div style={{ margin: '10px', width: '300px' }}>
+                <List
+                    bordered
+                    //关键代码-----------start
+                    dataSource={props.list}
+                    //关键代码-----------end
+                    renderItem={(item,index) => (<List.Item onClick={()=>props.deleteItem(index)} >{item}</List.Item>)}
+                />
+            </div>
+        </div>
+    );
 }
 export default TodoList;
